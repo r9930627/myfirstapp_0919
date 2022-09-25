b74aff24d37c6321bac1c35a1e33c032004b9d34
r9930627 <eunhaereal0718@gmail.com>
2022-09-26 01:21:06 +0800

    hi,flutter_0921

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,29 @@
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  var appTitle = Text('我的第一個Flutter app'),
+      hiFlutter = Text(
+        'hi,flutter.',
+        style: TextStyle(fontSize:50),
+      );
+
+  var appBody = Center(
+    child: hiFlutter,
+  );
+
+  var appBar = AppBar(
+    title: appTitle,
+  );
+
+  var app = MaterialApp(
+    home: Scaffold(
+      appBar: appBar,
+      body: appBody,
+    ),
+  );
+
+  runApp(app);
 }
 
 class MyApp extends StatelessWidget {
